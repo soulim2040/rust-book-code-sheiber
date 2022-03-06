5e545ff0415f2da98081dc6ae4a2b86d3b77ff6f
benny5609 <benny1315@163.com>
2022-03-06 20:56:07 +0800

    heap_sort

--- chapter6/src/main.rs
+++ chapter6/src/main.rs
@@ -188,9 +188,70 @@ fn select_sort(nums : &mut [i32]){
     }
 }
 
+macro_rules! parent {
+    ($child : ident) => {
+        $child >> 1
+    };
+}
+
+macro_rules! left_child {
+    ($parent  : ident) => {
+        ($parent << 1) + 1
+    };
+}
+
+macro_rules! right_child {
+    ($parent : ident) => {
+        ($parent + 1) << 1
+    };
+}
+
+fn heap_sort(nums : &mut [i32]){
+    if nums.len() <= 1 { return;}
+
+    let len = nums.len();
+
+    let last_parent = parent!(len);
+
+    for i in (0..=last_parent).rev(){
+        move_down(nums, i);
+    }
+
+    for end in (1..nums.len()).rev(){
+        nums.swap(0, end);
+        move_down(&mut nums[..end], 0)
+    }
+}
+
+//大者沉，小者浮
+fn move_down(nums : &mut [i32], mut parent : usize){
+    let last = nums.len() - 1;
+    loop {
+        let left = left_child!(parent);
+        let right = right_child!(parent);
+
+        if left > last {
+            break;
+        }
+
+        //取较大者
+        let child = if right <= last && nums[left] < nums[right]{
+            right
+        }else{
+            left
+        };
+
+        if nums[child] > nums[parent]{
+            nums.swap(parent, child);
+        }
+
+        parent = child;
+    }
+}
+
 fn main() {
     let mut nums = [11, 5, 22, 7, 12, 90, 76, 2];
-    select_sort(&mut nums);
+    heap_sort(&mut nums);
 
     dbg!(nums);   
 }
